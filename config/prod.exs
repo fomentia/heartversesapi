26a2ae8321fedc1bef309870c4e0949d010b635f
ijwilliams <isaac@thewilliams.ws>
2015-07-10 12:35:45 -0400

    phoenix.digest

--- config/prod.exs
+++ config/prod.exs
@@ -14,6 +14,7 @@ use Mix.Config
 config :heartversesapi, Heartversesapi.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com"],
+  cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
 #
