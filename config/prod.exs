7a6acf6b8db741a22e4f94a531b05d27a336a01f
ijwilliams <isaac@thewilliams.ws>
2015-07-10 13:29:51 -0400

    the ongoing quest for heroku sweetness continues

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :heartversesapi, Heartversesapi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"],
+  url: [host: "heartversesapi.herokuapp.com"],
   cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
