b0d472194d7e9f758175258f3065fadb068c6352
ijwilliams <isaac@thewilliams.ws>
2015-07-29 17:23:11 -0400

    Add the ability to parse passage titles with /api/v1/parse

--- web/controllers/v1/parse_controller.ex
+++ web/controllers/v1/parse_controller.ex
@@ -1,28 +1,51 @@
 defmodule Heartversesapi.V1.ParseController do
   use Heartversesapi.Web, :controller
+  alias Heartversesapi.Verses
 
   plug :action
 
   def index(conn, %{"passage" => passage}) do
     parsed_passage = PassageParser.parse(passage)
 
-    cond do
-      match?(%{book: book, chapter: chapter, verse_start: verse_start, verse_end: verse_end}, parsed_passage) ->
-        normalized_string = "#{parsed_passage[:book]} #{parsed_passage[:chapter]}:#{parsed_passage[:verse_start]}-#{parsed_passage[:verse_end]}"
-
-      match?(%{book: book, chapter: chapter, verse_start: verse_start}, parsed_passage) ->
-        normalized_string = "#{parsed_passage[:book]} #{parsed_passage[:chapter]}:#{parsed_passage[:verse_start]}"
-
-      match?(%{book: book, chapter: chapter}, parsed_passage) ->
-        normalized_string = "#{parsed_passage[:book]} #{parsed_passage[:chapter]}"
-
-      parsed_passage == "Invalid passage" ->
-        normalized_string = parsed_passage
-
-      true ->
-        normalized_string = "I'm sorry, but I had an internal error."
+    if parsed_passage != "Invalid passage" do
+      query_string = insert_sql_wildcards(parsed_passage[:book])
     end
 
-    render(conn, "index.json", parsed_passage: normalized_string)
+    cond do
+      match?(%{book: book, chapter: chapter, verse_start: verse_start, verse_end: verse_end}, parsed_passage) ->
+        %{book: book, chapter: chapter, verse_start: verse_start, verse_end: verse_end} = parsed_passage
+        full_passage = Repo.all(from v in Verses,
+                                where: fragment("LOWER(?) LIKE LOWER(?)", v.book, ^query_string),
+                                where: v.chapter == ^chapter,
+                                where: v.verse == ^verse_start)
+
+      match?(%{book: book, chapter: chapter, verse_start: verse_start}, parsed_passage) ->
+        %{book: book, chapter: chapter, verse_start: verse_start} = parsed_passage
+        full_passage = Repo.all(from v in Verses,
+                                where: fragment("LOWER(?) LIKE LOWER(?)", v.book, ^query_string),
+                                where: v.chapter == ^chapter,
+                                where: v.verse == ^verse_start)
+
+      match?(%{book: book, chapter: chapter}, parsed_passage) ->
+        %{book: book, chapter: chapter} = parsed_passage
+        full_passage = Repo.all(from v in Verses,
+                                where: fragment("LOWER(?) LIKE LOWER(?)", v.book, ^query_string),
+                                where: v.chapter == ^chapter)
+
+      parsed_passage == "Invalid passage" ->
+        full_passage = parsed_passage
+
+      true ->
+        full_passage = "I'm sorry, but I had an internal error."
+    end
+
+    render(conn, "index.json", parsed_passage: full_passage)
+  end
+
+  def insert_sql_wildcards(query_string) do
+    insert_percents = String.graphemes(query_string)
+                      |> Enum.map(fn(char) -> "%#{char}" end)
+                      |> List.to_string
+    insert_percents <> "%"
   end
 end
--- web/views/v1/parse_view.ex
+++ web/views/v1/parse_view.ex
@@ -1,8 +1,18 @@
 defmodule Heartversesapi.V1.ParseView do
   use Heartversesapi.Web, :view
 
+  def render("index.json", %{parsed_passage: "Invalid passage"}) do
+    %{passages: "Invalid passage"}
+  end
+
   def render("index.json", %{parsed_passage: parsed_passage}) do
-    %{:passage => parsed_passage}
+    Enum.map(parsed_passage, fn(passage) ->
+      %{
+        book: passage.book,
+        chapter: passage.chapter,
+        verse: passage.verse
+      }
+    end)
   end
 
 end
