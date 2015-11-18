35902ab832f2350f1ecf3ad0daae2f4614006194
ijwilliams <isaac@thewilliams.ws>
2015-11-18 15:18:58 -0500

    Fix #2

--- lib/passage_parser.ex
+++ lib/passage_parser.ex
@@ -1,17 +1,17 @@
 defmodule PassageParser do
   def parse(passage) do
-    match = Regex.run(~r/([A-Za-z]{2,})\s*(\d{1,3})(:(\d{1,3}))?(-(\d{1,3}))?/i, passage)
+    match = Regex.run(~r/([A-Za-z]{2,})\s*(\d{1,3}):?(\d{1,3})?-?(\d{1,3})?/i, passage)
 
     cond do
-      match?([_, book, chapter, _, verse_start, _, verse_end], match) ->
-        [_, book, chapter, _, verse_start, _, verse_end] = match
+      match?([_, book, chapter, verse_start, verse_end], match) ->
+        [_, book, chapter, verse_start, verse_end] = match
         %{:book => book,
           :chapter => String.to_integer(chapter),
           :verse_start => String.to_integer(verse_start),
           :verse_end => String.to_integer(verse_end)}
 
-      match?([_, book, chapter, _, verse], match) ->
-        [_, book, chapter, _, verse] = match
+      match?([_, book, chapter, verse], match) ->
+        [_, book, chapter, verse] = match
         %{:book => book, :chapter => String.to_integer(chapter), :verse_start => String.to_integer(verse)}
 
       match?([_, book, chapter], match) ->
