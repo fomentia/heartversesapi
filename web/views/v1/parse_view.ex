59f316e973ac780d3815215423748d1061bee674
ijwilliams <isaac@thewilliams.ws>
2015-11-24 16:00:11 -0500

    yo

--- web/views/v1/parse_view.ex
+++ web/views/v1/parse_view.ex
@@ -8,24 +8,26 @@ defmodule Heartversesapi.V1.ParseView do
 
   # Handle single-verse parse requests
   def render("index.json", %{parsed_passage: [passage | rest]}) when rest == [] do
-    %{
-      book: passage.book,
-      chapter: passage.chapter,
-      verse: passage.verse
-    }
+    "yo"
+    # %{
+    #   book: passage.book,
+    #   chapter: passage.chapter,
+    #   verse: passage.verse
+    # }
   end
 
   # Handle range parse requests
   def render("index.json", %{parsed_passage: parsed_passage}) do
-    first_passage = List.first(parsed_passage)
-    last_passage = List.last(parsed_passage)
-
-    %{
-      book: first_passage.book,
-      chapter: first_passage.chapter,
-      verse_start: first_passage.verse,
-      verse_end: last_passage.verse
-    }
+    "yo"
+    # first_passage = List.first(parsed_passage)
+    # last_passage = List.last(parsed_passage)
+    #
+    # %{
+    #   book: first_passage.book,
+    #   chapter: first_passage.chapter,
+    #   verse_start: first_passage.verse,
+    #   verse_end: last_passage.verse
+    # }
   end
 
 end
